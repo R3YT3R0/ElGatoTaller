f76b6fc1e958d965c2382a2385e8f63546b2f494
R3YT3R0 <112895142+R3YT3R0@users.noreply.github.com>
2023-03-24 22:38:03 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,24 +1,33 @@
+
+
+var wrapper = document.querySelector(".wrapper");
+
+document.getElementById("BotonContacto").addEventListener("click", body2)
+
+document.getElementById("icon-close").addEventListener("click", body4);
+
+var body = document.getElementById("Difuminado");
+
+
+function body2 (){
+  body.classList.add("Difuminado");
+}
+
+function body4 (){
+  body.classList.remove("Difuminado");
+}
+
 document
   .getElementById("BotonFlecha")
   .addEventListener("click", menuDesplegable);
 
 function menuDesplegable() {
   var menu = document.getElementById("mapa");
-  var flecha = document.getElementById("BotonFlecha");
 
   if (menu.className == "mapaCerrado") {
-    menu.className = "mapaAbierto";
-    flecha.className = "bx bxs-up-arrow";
+    menu.classList.add("mapaAbierto")
   } else {
-    menu.className = "mapaCerrado";
-    flecha.className = "bx bxs-down-arrow";
+    menu.classList.remove("mapaAbierto")
   }
 }
 
-document.getElementById("Cancel").addEventListener("click", resetForm);
-
-function resetForm() {
-  var Form = document.getElementById("FormularioSeccion2");
-
-  Form.reset();
-}
